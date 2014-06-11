0d6dd55af319c3c057ab6e711df7f9cb63afe78f
Dan Kolbman <dankolbman@gmail.com>
2014-06-11 14:34:38 +0000

    Scale g(r) to half the radius

--- src/grinit.f
+++ src/grinit.f
@@ -10,7 +10,7 @@ c       -i,j = loop indices
 c       Set the bin width.
         delta=1.0/20.0
         if(circ .eq. 1) then
-          delta=1.0/(radius/2.0)
+          delta=1.0/(radius*2.0)
         endif
         invdelta=1.0/delta
         
